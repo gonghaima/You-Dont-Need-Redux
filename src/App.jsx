29940f5a69dec405e80f0e73d0cfa1118c42eaf3
Steven <gonghaima@hotmail.com>
2019-04-18 00:50:54 +1000

    add Action

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,19 @@ import { Store } from "./Store";
 
 export default function App() {
   const { state, dispatch } = React.useContext(Store);
+  const fetchDataAction = async () => {
+    const data = await fetch(
+      "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes"
+    );
+    const dataJSON = await data.json();
+    return dispatch({
+      type: "FETCH_DATA",
+      payload: dataJSON._embedded.episodes
+    });
+  };
+  React.useEffect(() => {
+    state.episodes.length === 0 && fetchDataAction();
+  });
   return (
     <React.Fragment>
       <div>
