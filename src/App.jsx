84f7c85ccbb5dddc6bd959d700d6797e53993eca
Steven <gonghaima@hotmail.com>
2019-04-18 00:45:13 +1000

    destructure store into state, dispatch

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import { Store } from "./Store";
 
 export default function App() {
-  const store = React.useContext(Store);
-
+  const { state, dispatch } = React.useContext(Store);
   return (
     <React.Fragment>
-      {console.log(store)}
       <div>
         <h1>Rick and Morty</h1>
         <p>Pick your favourite episodes</p>
