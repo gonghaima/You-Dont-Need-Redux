ef4da4ed96493046f2d1abc06734995d07417586
Steven <gonghaima@hotmail.com>
2019-04-18 01:26:38 +1000

    Reach/Router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "@reach/router";
 import { Store } from "./Store";
 const EpisodesList = React.lazy(() => import("./EpisodesList"));
 
@@ -48,7 +49,10 @@ export default function App() {
             <h1>Rick and Morty</h1>
             <p>Pick your favourite episodes</p>
           </div>
-          <div>Favourite(s) {state.favourites.length}</div>
+          <div>
+            <Link to="/">Home</Link>{" "}
+            <Link to="/faves">Favourite(s) {state.favourites.length}</Link>
+          </div>
         </header>
         <section className="episode-layout">
           <EpisodesList {...props} />
