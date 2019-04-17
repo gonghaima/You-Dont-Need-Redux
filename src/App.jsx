63707828a2f62ddab7edaf726eed8f8765e31ef7
Steven <gonghaima@hotmail.com>
2019-04-18 01:23:04 +1000

    splittin code && suspense and lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Store } from "./Store";
+const EpisodesList = React.lazy(() => import("./EpisodesList"));
 
 export default function App() {
   const { state, dispatch } = React.useContext(Store);
@@ -33,41 +34,26 @@ export default function App() {
   React.useEffect(() => {
     state.episodes.length === 0 && fetchDataAction();
   });
+  const props = {
+    episodes: state.episodes,
+    toggleFavAction: toggleFavAction,
+    favourites: state.favourites
+  };
   return (
     <React.Fragment>
-      {console.log(state)}
-      <header className="header">
-        <div>
-          <h1>Rick and Morty</h1>
-          <p>Pick your favourite episodes</p>
-        </div>
-        <div>Favourite(s) {state.favourites.length}</div>
-      </header>
-      <section className="episode-layout">
-        {state.episodes.map(episode => {
-          return (
-            <section key={episode.id} className="episode-box">
-              <img
-                src={episode.image.medium}
-                alt={`Rick and Morty ${episode.name}`}
-              />
-              <div>{episode.name}</div>
-              <section
-                style={{ display: "flex", justifyContent: "space-between" }}
-              >
-                <div>
-                  Season: {episode.season} Number: {episode.number}
-                </div>
-                <button type="button" onClick={() => toggleFavAction(episode)}>
-                  {state.favourites.find(fav => fav.id === episode.id)
-                    ? "Unfav"
-                    : "Fav"}
-                </button>
-              </section>
-            </section>
-          );
-        })}
-      </section>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        {console.log(state)}
+        <header className="header">
+          <div>
+            <h1>Rick and Morty</h1>
+            <p>Pick your favourite episodes</p>
+          </div>
+          <div>Favourite(s) {state.favourites.length}</div>
+        </header>
+        <section className="episode-layout">
+          <EpisodesList {...props} />
+        </section>
+      </React.Suspense>
     </React.Fragment>
   );
 }
---
+++ src/EpisodesList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function EpisodesList(props) {
+  const { episodes, toggleFavAction, favourites } = props;
+
+  return episodes.map(episode => {
+    return (
+      <section key={episode.id} className="episode-box">
+        <img
+          src={episode.image.medium}
+          alt={`Rick and Morty ${episode.name}`}
+        />
+        <div>{episode.name}</div>
+        <section style={{ display: "flex", justifyContent: "space-between" }}>
+          <div>
+            Season: {episode.season} Number: {episode.number}
+          </div>
+          <button type="button" onClick={() => toggleFavAction(episode)}>
+            {favourites.find(fav => fav.id === episode.id) ? "Unfav" : "Fav"}
+          </button>
+        </section>
+      </section>
+    );
+  });
+}
